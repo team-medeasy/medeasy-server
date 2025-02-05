a0b0cfaedbdc3c9d52f8a472bf932eac362eb059
jiwon <angry9908@gmail.com>
2025-02-05 19:07:44 +0900

    setting: init project

---
+++ src/main/java/com/medeasy/MedeasyApplication.java
@@ -0,0 +1,13 @@
+package com.medeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medeasy/MedeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
