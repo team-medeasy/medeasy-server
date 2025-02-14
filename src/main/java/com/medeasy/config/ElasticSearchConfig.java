f070611fe19ae68c500cd1ae95425f933d8e4dfe
jiwon <angry9908@gmail.com>
2025-02-14 22:24:59 +0900

    refactor: git action workflow add env

--- src/main/java/com/medeasy/config/ElasticSearchConfig.java
+++ src/main/java/com/medeasy/config/ElasticSearchConfig.java
@@ -1,9 +1,5 @@
 package com.medeasy.config;
 
-import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.message.BasicHeader;
-import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -13,7 +9,7 @@ import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfigurat
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     @Value("${spring.data.elasticsearch.url}")
-    String url;
+    private String url;
 
     @Override
     public ClientConfiguration clientConfiguration() {
