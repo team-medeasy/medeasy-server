d20bdb7b9086b61ea7389549ca33a0cc5e3a47c6
jiwon <angry9908@gmail.com>
2025-02-18 17:11:26 +0900

    settig: document 초기화 삭제

--- src/main/java/com/medeasy/domain/medicine/service/MedicineDocumentService.java
+++ src/main/java/com/medeasy/domain/medicine/service/MedicineDocumentService.java
@@ -22,7 +22,7 @@ public class MedicineDocumentService {
     // 애플리케이션 실행시 elasticsearch repository, repo 동기화 작업
     @PostConstruct
     public void init() {
-        indexAllMedicines();
+        //indexAllMedicines();
     }
 
     public void indexAllMedicines() {
