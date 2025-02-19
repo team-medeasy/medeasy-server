a5463a286f2a18dccf3078a2fc407d1a8e6eac57
jiwon <angry9908@gmail.com>
2025-02-19 10:06:10 +0900

    refactor: health 체크 인증 제외

--- src/main/java/com/medeasy/domain/health/HealthController.java
+++ src/main/java/com/medeasy/domain/health/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/open-api/health")
 public class HealthController {
 
     @GetMapping("")
---
+++ src/main/java/com/medeasy/domain/routine/controller/RoutineController.java
@@ -0,0 +1,13 @@
+package com.medeasy.domain.routine.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/routine")
+@RequiredArgsConstructor
+public class RoutineController {
+
+
+}
