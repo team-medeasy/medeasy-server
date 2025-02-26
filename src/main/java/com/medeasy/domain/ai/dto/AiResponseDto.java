8ce2fd7c80b3367d6dbb06c7e3273b9245a01efe
jiwon <angry9908@gmail.com>
2025-02-26 21:02:09 +0900

    feat: gemini 요청 dto 추가

--- src/main/java/com/medeasy/domain/ai/dto/AiResponseDto.java
+++ src/main/java/com/medeasy/domain/ai/dto/AiResponseDto.java
@@ -2,10 +2,7 @@ package com.medeasy.domain.ai.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -20,15 +17,17 @@ public class AiResponseDto {
     @JsonProperty("results")
     private List<DoseDto> doseDtos;
 
+    @Getter
+    @Setter
     public static class DoseDto{
         @JsonProperty("name")
         private String name;
         @JsonProperty("dose")
-        private Integer dose;
+        private Integer dose; // 1회 투약량
         @JsonProperty("type_count")
-        private Integer typeCount;
+        private Integer typeCount; // 1일 투여횟수
         @JsonProperty("total_days")
-        private Integer totalDays;
+        private Integer totalDays; // 총 투약일수
         @JsonProperty("use_method")
         private String useMethod;
     }
