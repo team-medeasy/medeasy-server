c819ea3e988ac3f4ee0f793a1fa6a25719a7ee50
jiwon <angry9908@gmail.com>
2025-03-04 18:35:43 +0900

    docs: swagger 설명 추가

--- src/main/java/com/medeasy/domain/medicine/controller/MedicineController.java
+++ src/main/java/com/medeasy/domain/medicine/controller/MedicineController.java
@@ -64,22 +64,33 @@ public class MedicineController {
 
     // 약 검색 색상 필터링
     @GetMapping("/search")
-    @Operation(summary = "약 검색 API v2", description = "검색엔진 기반으로 약 이름 검색시 유사한 약 리스트 출력")
+    @Operation(summary = "약 검색 API v2", description =
+            """
+            약 검색 API:
+            
+            약 이름, 색상, 모양 조건 선택하여 검색 
+                
+            약 색상, 모양 여러개 조건 선택시 ex) 빨강, 노랑 
+            
+            빨간약, 노랸약, 빨간노란약 출력 
+            
+            이외의 조건은 지속적으로 추가 예정 
+            """)
     public Api<List<MedicineResponse>> searchMedicinesWithColor(
             @RequestParam(value = "name", required = false)
-            @Parameter(description = "약 이름 키워드", required = false)
+            @Parameter(description = "약 이름 키워드 (nullable)", required = false)
             String name,
 
             @RequestParam(value = "colors", required = false)
-            @Parameter(description = "약 색상 필터링 (nullable) ex) 하양, 노랑, 분홍, 파랑, 금, 기타", required = false)
+            @Parameter(description = "약 색상 필터링 (nullable)", required = false)
             List<MedicineColor> colors,
 
             @RequestParam(value = "shape", required = false)
-            @Parameter(description = "약 모양 (nullable) ex) 원형, 삼각형 ...", required = false)
+            @Parameter(description = "약 모양 필터링 (nullable)", required = false)
             List<MedicineShape> shapes,
 
-            @RequestParam(value = "size", defaultValue = "10")
-            @Parameter(description = "불러올 데이터 개수", required = false)
+            @RequestParam(value = "size", defaultValue = "10", required = false)
+            @Parameter(description = "불러올 데이터 개수 (default: 10)", required = false)
             int size
     ) {
         List<MedicineResponse> medicineResponses= medicineBusiness.searchMedicinesWithColor(name, colors, shapes, size);
