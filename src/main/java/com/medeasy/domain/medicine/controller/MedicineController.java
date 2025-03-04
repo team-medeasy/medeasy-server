16bf779f8b3459cfe80ad4a9f6b13d89c6312114
jiwon <angry9908@gmail.com>
2025-03-04 17:27:46 +0900

    feat: swagger 문서 수정

--- src/main/java/com/medeasy/domain/medicine/controller/MedicineController.java
+++ src/main/java/com/medeasy/domain/medicine/controller/MedicineController.java
@@ -2,6 +2,7 @@ package com.medeasy.domain.medicine.controller;
 
 import com.medeasy.common.api.Api;
 import com.medeasy.domain.medicine.business.MedicineBusiness;
+import com.medeasy.domain.medicine.db.MedicineColor;
 import com.medeasy.domain.medicine.dto.MedicineRequest;
 import com.medeasy.domain.medicine.dto.MedicineResponse;
 import com.medeasy.domain.medicine.dto.MedicineUpdateRequest;
@@ -64,13 +65,13 @@ public class MedicineController {
     @GetMapping("/search")
     @Operation(summary = "약 검색 API v2", description = "검색엔진 기반으로 약 이름 검색시 유사한 약 리스트 출력")
     public Api<List<MedicineResponse>> searchMedicinesWithColor(
-            @RequestParam(value = "name", required = true)
-            @Parameter(description = "약 이름 키워드", required = true)
+            @RequestParam(value = "name", required = false)
+            @Parameter(description = "약 이름 키워드", required = false)
             String name,
 
             @RequestParam(value = "colors", required = false)
             @Parameter(description = "약 색상 필터링 (nullable) ex) 하양, 노랑, 분홍, 파랑, 금, 기타", required = false)
-            List<String> colors,
+            List<MedicineColor> colors,
 
             @RequestParam(value = "shape", required = false)
             @Parameter(description = "약 모양 (nullable) ex) 원형, 삼각형 ...", required = false)
