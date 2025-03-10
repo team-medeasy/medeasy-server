a0713e7734d0ac409785f8c4e88cc5cb11b8493c
jiwon <angry9908@gmail.com>
2025-03-10 19:00:13 +0900

    docs: search history swagger 명세 추가

--- src/main/java/com/medeasy/domain/search/controller/SearchHistoryController.java
+++ src/main/java/com/medeasy/domain/search/controller/SearchHistoryController.java
@@ -19,7 +19,19 @@ public class SearchHistoryController {
     private final SearchHistoryBusiness searchHistoryBusiness;
 
     @GetMapping("")
-    @Operation(summary = "사용자 최근 검색어 조회 api", description = "사용자 최근 검색어 조회 api")
+    @Operation(summary = "사용자 최근 검색어 조회 api", description =
+            """
+            사용자 최근 검색어 조회 api
+            
+            size 개수만큼 사용자 검색 기록을 조회한다. 
+            
+            반환값: 
+            
+            id: 문자열 -> 사용자 검색어 식별자 나중에 검색어 삭제 api 호출시 활용 
+            
+            keyword: 문자열 -> 사용자 검색 기록 키워드
+            """
+    )
     public Object getUserSearchHistory(
             @Parameter(hidden = true) @UserSession Long userId,
             @RequestParam(value = "size", defaultValue = "10", required = false)
@@ -32,7 +44,15 @@ public class SearchHistoryController {
     }
 
     @DeleteMapping("")
-    @Operation(summary = "사용자 최근 검색어 삭제 api", description = "사용자 최근 검색어 삭제 api")
+    @Operation(summary = "사용자 최근 검색어 삭제 api", description =
+            """
+            사용자 최근 검색어 삭제 api
+            
+            사용자 검색어 조회 api를 통하여 조회된 문자열 id를 입력하여, 
+            
+            해당 검색어 삭제 
+            """
+    )
     public Object deleteUserSearchHistory(
             @Parameter(hidden = true) @UserSession Long userId,
             @RequestParam(value = "search_history_id", required = true)
