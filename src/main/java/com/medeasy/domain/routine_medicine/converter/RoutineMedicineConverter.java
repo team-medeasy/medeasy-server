a9d1b8fa5d1167466bba207c082cb9baec817bba
jiwon <angry9908@gmail.com>
2025-03-16 03:59:49 +0900

    feat: routine medicine converter 추가

---
+++ src/main/java/com/medeasy/domain/routine_medicine/converter/RoutineMedicineConverter.java
@@ -0,0 +1,20 @@
+package com.medeasy.domain.routine_medicine.converter;
+
+import com.medeasy.common.annotation.Converter;
+import com.medeasy.domain.routine_medicine.db.RoutineMedicineEntity;
+import com.medeasy.domain.routine_medicine.dto.RoutineMedicineDto;
+
+@Converter
+public class RoutineMedicineConverter {
+
+    public RoutineMedicineDto toDto(RoutineMedicineEntity routineMedicineEntity) {
+        return RoutineMedicineDto.builder()
+                .id(routineMedicineEntity.getId())
+                .nickname(routineMedicineEntity.getNickname())
+                .medicineId(routineMedicineEntity.getMedicineId())
+                .dose(routineMedicineEntity.getDose())
+                .isTaken(routineMedicineEntity.getIsTaken())
+                .build()
+                ;
+    }
+}
