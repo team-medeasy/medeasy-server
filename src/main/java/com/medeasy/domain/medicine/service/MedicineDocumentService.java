c29e5c2990ca4e9859f3a12a285fe61815e02d9a
jiwon <angry9908@gmail.com>
2025-03-17 14:36:52 +0900

    refactor: 이전 버전 검색 로직 삭제

--- src/main/java/com/medeasy/domain/medicine/service/MedicineDocumentService.java
+++ src/main/java/com/medeasy/domain/medicine/service/MedicineDocumentService.java
@@ -18,56 +18,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MedicineDocumentService {
-
-    private static final Logger log = LoggerFactory.getLogger(MedicineDocumentService.class);
     private final MedicineSearchRepository medicineSearchRepository;
-    private final MedicineRepository medicineRepository; // 기존 JPA Repository
-
     private final MedicineSearchCustomRepository medicineSearchCustomRepository;
 
-
-    // 애플리케이션 실행시 elasticsearch repository, repo 동기화 작업
-////    @PostConstruct
-//    public void init() {
-//        indexAllMedicines();
-//    }
-//
-//    public void indexAllMedicines() {
-//        List<MedicineEntity> medicines = medicineRepository.findAll();
-//        List<MedicineDocument> medicineDocuments = medicines.stream()
-//                .map(m -> MedicineDocument.builder()
-//                        .id(m.getId().toString())
-//                        .entpName(m.getEntpName())
-//                        .itemName(m.getItemName())
-//                        .depositMethod(m.getDepositMethod())
-//                        .openAt(m.getOpenAt())
-//                        .updateAt(m.getUpdateAt())
-//                        .imageUrl(m.getImageUrl())
-//                        .bizrno(m.getBizrno())
-//                        .build()
-//                ).toList();
-//
-//        int batchSize = 100; // ✅ 배치 크기 조절 (500개씩 처리)
-//        for(int i=0; i<medicineDocuments.size(); i+=batchSize) {
-//            List<MedicineDocument> batch = medicineDocuments.subList(i, Math.min(i+batchSize, medicineDocuments.size()));
-//            medicineSearchRepository.saveAll(batch);
-//
-//            log.info(" {}번째 배치 성공", i+1);
-//        }
-//    }
-
-    // TODO 검색한 약이 존재하지 않을 경우 크롤링 고려
-    public List<MedicineDocument> searchMedicineContainingName(String medicineName, int size) {
-        log.info("약 검색 {}", medicineName);
-
-        Pageable pageable = PageRequest.of(0, size);
-        List<MedicineDocument> medicineDocuments=medicineSearchRepository.findByItemNameContaining(medicineName, pageable);
-        medicineDocuments.stream()
-                .findAny()
-                .orElseThrow(()-> new ApiException(MedicineErrorCode.NOT_FOUND_MEDICINE, "해당하는 약이 존재하지 않습니다. "+medicineName))
-                ;
-        return medicineDocuments;
-    }
 
     public List<MedicineDocument> searchMedicineContainingNameWithColor(String medicineName, List<String> colors, List<String> shape, int size) {
         Pageable pageable = PageRequest.of(0, size);
