942356af859aa2b9604845748dfc2e67d862cc57
jiwon <angry9908@gmail.com>
2025-03-17 17:05:04 +0900

    feat: 사용자 스케줄 dto 추가

---
+++ src/main/java/com/medeasy/domain/user_schedule/dto/UserScheduleDto.java
@@ -0,0 +1,21 @@
+package com.medeasy.domain.user_schedule.dto;
+
+import com.medeasy.domain.routine_medicine.dto.RoutineMedicineDto;
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserScheduleDto {
+
+    private Long userScheduleId;
+
+    private String name;
+
+    private LocalTime takeTime;
+}
