4e0d82bee574f98ef4582f155328dfed4921555e
jiwon <angry9908@gmail.com>
2025-03-17 18:27:18 +0900

    docs: 스케줄 조회 api 스웨거 설명 추가

--- src/main/java/com/medeasy/domain/user/controller/UserController.java
+++ src/main/java/com/medeasy/domain/user/controller/UserController.java
@@ -7,12 +7,15 @@ import com.medeasy.domain.user.business.UserBusiness;
 import com.medeasy.domain.user.dto.UserDeleteRequest;
 import com.medeasy.domain.user.dto.UserScheduleResponse;
 import com.medeasy.domain.user.dto.UserUsageDaysResponse;
+import com.medeasy.domain.user_schedule.dto.UserScheduleDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -48,23 +51,29 @@ public class UserController {
         return Api.OK(response);
     }
 
-    @Operation(summary = "사용자 루틴 스케줄 조회 api", description =
+    @Operation(summary = "사용자 루틴 스케줄 조회 api v2", description =
             """
-            사용자 루틴 스케줄 조회 API:
-            
-            루틴을 등록할 떼 사용되는 사용자의 스케줄 시간 조회
+                사용자 루틴 스케줄 조회 API:
+                
+                루틴을 등록할 떼 사용되는 사용자의 스케줄 시간 조회
             
             응답 값 설명: 
             
-            morning(아침 시간), lunch(점심 시간), dinner(저녁 시간), bed_time(취침 시간) 
+            - user_schedule_id: 사용자 스케줄 식별 ID
+            
+            - name: 스케줄 이름
+            
+            - take_time: 복용 시간
+            
+            마지막 업데이트: 3/17
             """
     )
-    @GetMapping("/schdule")
+    @GetMapping("/schedule")
     public Api<Object> getRoutineSchedule(
             @Parameter(hidden = true)
             @UserSession Long userId
     ) {
-        UserScheduleResponse response=userBusiness.getRoutineSchedule(userId);
+        List<UserScheduleDto> response=userBusiness.getRoutineSchedule(userId);
 
         return Api.OK(response);
     }
@@ -141,4 +150,6 @@ public class UserController {
 
         return Api.OK(response);
     }
+
+
 }
