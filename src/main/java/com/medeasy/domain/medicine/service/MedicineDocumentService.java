205d076c97621823d27569dd668b867d221446a3
jiwon <angry9908@gmail.com>
2025-03-17 16:33:43 +0900

    feat: 약품 조회 서비스 코드 추가

--- src/main/java/com/medeasy/domain/medicine/service/MedicineDocumentService.java
+++ src/main/java/com/medeasy/domain/medicine/service/MedicineDocumentService.java
@@ -1,18 +1,13 @@
 package com.medeasy.domain.medicine.service;
 
-import com.medeasy.common.api.Api;
 import com.medeasy.common.error.MedicineErrorCode;
 import com.medeasy.common.exception.ApiException;
 import com.medeasy.domain.medicine.db.*;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -34,5 +29,25 @@ public class MedicineDocumentService {
 
     public MedicineDocument findMedicineDocumentById(String id) {
         return medicineSearchRepository.findById(id).orElseThrow(() -> new ApiException(MedicineErrorCode.NOT_FOUND_MEDICINE));
+    }
+
+    /**
+     * EDI_CODE를 통해 정확한 단일 약품 조회
+     * */
+    public MedicineDocument findMedicineDocumentByEdiCode(String ediCode) {
+        return medicineSearchCustomRepository.findByEdiCode(ediCode);
+    }
+
+    /**
+     * EDI_CODE로 정확한 약품 검색이 어려울 때, 이름이 유사한 약품 검색
+     * */
+    public List<MedicineDocument> findMedicineByEdiCodeAndItemName(String ediCode, String itemName, int size) {
+        Pageable pageable = PageRequest.of(0, size);
+        List<MedicineDocument> medicineDocuments=medicineSearchCustomRepository.findMedicineByEdiCodeAndItemName(ediCode, itemName, pageable);
+        medicineDocuments.stream()
+                .findAny()
+                .orElseThrow(() -> new ApiException(MedicineErrorCode.NOT_FOUND_MEDICINE, "일치하는 약이 존재하지 않습니다"));
+
+        return medicineDocuments;
     }
 }
