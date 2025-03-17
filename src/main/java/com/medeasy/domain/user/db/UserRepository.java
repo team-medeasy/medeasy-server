c6d4b0a482e923fa3dfa2fe88b9f3b155b23d97e
jiwon <angry9908@gmail.com>
2025-03-17 17:25:30 +0900

    feat: user fetch join query 추가

--- src/main/java/com/medeasy/domain/user/db/UserRepository.java
+++ src/main/java/com/medeasy/domain/user/db/UserRepository.java
@@ -1,6 +1,8 @@
 package com.medeasy.domain.user.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +10,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+
+    @Query("SELECT u from UserEntity u " +
+            "LEFT JOIN FETCH u.userSchedules " +
+            "WHERE u.id = :userId")
+    Optional<UserEntity> findByIdToFetchJoin(
+            @Param("userId") Long userId
+    );
 }
