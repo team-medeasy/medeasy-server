b127b4ca40428355532c9b03e22b431416eb6766
jiwon <angry9908@gmail.com>
2025-03-21 21:42:10 +0900

    feat: 잘못된 형식의 Json 데이터 에러 핸들러 추가

--- src/main/java/com/medeasy/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/medeasy/exceptionhandler/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import com.medeasy.common.api.Api;
 import com.medeasy.common.error.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice // 예외처리 가져오기
 @Order(Integer.MAX_VALUE) // 값이 높을 수록 낮은 우선순위 즉 나중에 실행 //디폴트가 맥스
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Api<Object>> handleInvalidJson(HttpMessageNotReadableException ex) {
+        Api<Object> errorResponse = Api.ERROR(ErrorCode.BAD_REQEUST, "요청 형식이 올바르지 않습니다 (JSON 파싱 오류)");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
     @ExceptionHandler(value=Exception.class) // 예외 전부 //커스텀 예외로 앞에서 다 잡기 때문에 이곳은 예상치 못한 예외 잡힘
     public ResponseEntity<Api<Object>> exception(
