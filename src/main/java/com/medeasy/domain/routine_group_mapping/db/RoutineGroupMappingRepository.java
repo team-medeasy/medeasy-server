16983a35194a040567425f2a5fdfdb028fafb7ec
jiwon <angry9908@gmail.com>
2025-04-08 01:21:58 +0900

    feat: routine_group_mapping entity

---
+++ src/main/java/com/medeasy/domain/routine_group_mapping/db/RoutineGroupMappingRepository.java
@@ -0,0 +1,6 @@
+package com.medeasy.domain.routine_group_mapping.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutineGroupMappingRepository extends JpaRepository<RoutineGroupMappingEntity, Long> {
+}
