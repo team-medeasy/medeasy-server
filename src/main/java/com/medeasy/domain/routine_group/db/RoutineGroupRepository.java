34d9029394c52ca7bf497ceb294e6d84ee60bed2
jiwon <angry9908@gmail.com>
2025-04-18 03:31:45 +0900

    feat: 루틴 그룹 쿼리 추가

--- src/main/java/com/medeasy/domain/routine_group/db/RoutineGroupRepository.java
+++ src/main/java/com/medeasy/domain/routine_group/db/RoutineGroupRepository.java
@@ -1,6 +1,23 @@
 package com.medeasy.domain.routine_group.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RoutineGroupRepository extends JpaRepository<RoutineGroupEntity, Long> {
+
+    @Query("SELECT rg FROM RoutineGroupEntity rg " +
+            "JOIN rg.routines r " +
+            "WHERE r.id=:routineId " +
+            "AND rg.user.id=:userId")
+    Optional<RoutineGroupEntity> findByRoutineIdAndUserId(Long routineId, Long userId);
+
+    @Query("SELECT DISTINCT rg FROM RoutineGroupEntity rg " +
+            "JOIN FETCH rg.routines r " +
+            "JOIN FETCH r.userSchedule us " +
+            "WHERE us.user.id=:userId " +
+            "AND r.id = :routineId ")
+    Optional<RoutineGroupEntity> findRoutineGroupContainsRoutineIdByUserId(@Param("userId") Long userId, @Param("routineId") Long routineId);
 }
