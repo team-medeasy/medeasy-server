4bf35bc84a32158b820b61ad7d8b24890c43a298
jiwon <angry9908@gmail.com>
2025-04-25 04:21:27 +0900

    feat: workflow add gcp credential

--- src/test/java/com/medeasy/MedeasyApplicationTests.java
+++ src/test/java/com/medeasy/MedeasyApplicationTests.java
@@ -1,24 +1,14 @@
 package com.medeasy;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
+@SpringBootTest
 class MedeasyApplicationTests {
-	@TestConfiguration
-	static class TestStorageConfig {
-		@Bean
-		public com.google.cloud.storage.Storage storage() {
-			return Mockito.mock(com.google.cloud.storage.Storage.class);
-		}
-	}
 
 	@Test
-	void contextLoads() {}
+	void contextLoads() {
+		// storage 가 목이기 때문에 자격증명 오류 없이 컨텍스트 로드
+	}
 }
-
-
 
