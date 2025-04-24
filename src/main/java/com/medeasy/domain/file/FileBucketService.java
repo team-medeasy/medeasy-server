66d5fff9805a66955e128c632f906ddb41af5953
jiwon <angry9908@gmail.com>
2025-04-24 19:52:26 +0900

    feat: bites to mp3 util interface

---
+++ src/main/java/com/medeasy/domain/file/FileBucketService.java
@@ -0,0 +1,8 @@
+package com.medeasy.domain.file;
+
+import java.io.File;
+
+public interface FileBucketService {
+
+    void saveMp3File(File mp3File);
+}
---
+++ src/main/java/com/medeasy/domain/file/GcpFileBucketService.java
@@ -0,0 +1,14 @@
+package com.medeasy.domain.file;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class GcpFileBucketService implements FileBucketService {
+
+    @Override
+    public void saveMp3File(File mp3File) {
+
+    }
+}
