e9a34b2c729d7e4e2003e49a5ad63ef4070fb1ed
jiwon <angry9908@gmail.com>
2025-04-25 03:22:25 +0900

    refactor: component annotation

--- src/main/java/com/medeasy/domain/medicine/util/DefaultMedicineInfoGenerator.java
+++ src/main/java/com/medeasy/domain/medicine/util/DefaultMedicineInfoGenerator.java
@@ -1,7 +1,10 @@
 package com.medeasy.domain.medicine.util;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public class DefaultMedicineInfoGenerator extends MedicineInfoGenerator{
     @Override
     String generateMedicineInfoTitleScript(String medicineName) {
