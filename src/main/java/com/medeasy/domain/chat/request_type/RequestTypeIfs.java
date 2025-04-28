bd938cf6397736acd87bbf79c46c9435e9a755bb
jiwon <angry9908@gmail.com>
2025-04-29 00:25:55 +0900

    feat: routine type ifs

---
+++ src/main/java/com/medeasy/domain/chat/request_type/RequestTypeIfs.java
@@ -0,0 +1,8 @@
+package com.medeasy.domain.chat.request_type;
+
+public interface RequestTypeIfs {
+    String getCode();
+    String getType();
+    String getSummary();
+    String getCondition();
+}
