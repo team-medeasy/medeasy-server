056530cfd2301b6642d8877b8dd5ae1eaa3d5591
jiwon <angry9908@gmail.com>
2025-05-06 14:17:33 +0900

    refactor: rm socket config

--- src/main/java/com/medeasy/config/WebSocketConfig.java
+++ src/main/java/com/medeasy/config/WebSocketConfig.java
@@ -1,25 +1,24 @@
-package com.medeasy.config;
-
-import com.medeasy.domain.chat.handler.ChatWebSocketHandler;
-import com.medeasy.interceptor.AuthHandshakeInterceptor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-@Configuration
-@EnableWebSocket
-@RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final ChatWebSocketHandler chatWebSocketHandler;
-    private final AuthHandshakeInterceptor authHandshakeInterceptor;
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "/ws/chat")
-                .addInterceptors(authHandshakeInterceptor)
-                .setAllowedOrigins("*"); // 개발 단계에서는 허용. 실제 운영은 주의
-    }
-}
+//package com.medeasy.config;
+//
+//import com.medeasy.domain.chat.handler.ChatWebSocketHandler;
+//import com.medeasy.interceptor.AuthHandshakeInterceptor;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//@Configuration
+//@EnableWebSocket
+//@RequiredArgsConstructor
+//public class WebSocketConfig implements WebSocketConfigurer {
+//
+//    private final ChatWebSocketHandler chatWebSocketHandler;
+//    private final AuthHandshakeInterceptor authHandshakeInterceptor;
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(chatWebSocketHandler, "/ws/chat")
+//                .addInterceptors(authHandshakeInterceptor);
+//    }
+//}
 
